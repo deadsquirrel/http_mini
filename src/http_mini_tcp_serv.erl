2a6bf11fdf8d5ff2e6c84159887ae1f9780cdff7
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2015-11-24 19:37:08 +0200

    parsing v.1

--- src/http_mini_tcp_serv.erl
+++ src/http_mini_tcp_serv.erl
@@ -81,7 +81,9 @@ go_recv (Sock) ->
             io:format("Head = ~p~n", [H]),
             io:format("Tail = ~p~n", [T]),
             if H == "GET" ->
-                    parsing:pars(Stroka);
+%% ругается ана следующую строку      
+%%  {http_mini_tcp_serv,go_recv,1,[{file,"src/http_mini_tcp_serv.erl"}    
+                    parsing(Stroka);
                true -> xpenb
             end,
             
@@ -101,6 +103,44 @@ go_recv (Sock) ->
 
             %% _ ->
             %%     ok = gen_tcp:close(Sock)
+%%%===================================================================
+%%% парсим полученные строки 
+%%%===================================================================
+parsing (List) -> 
+    List2 = string:tokens(List, "\r\n"),
+    io:format("List2=~p~n", [List2]),
+    pars1(List2, 1, []).
+
+
+pars1 ([], _K, Acc) -> Acc;
+pars1 ([H|T], K, Acc) ->
+    if 
+        K==1 ->
+            Str = pars_st1 (string:tokens(H, " ")),
+%% что-то тут енне так 
+%%  {{badmatch,[{"GET","/"}]},
+%% [{http_mini_tcp_serv,pars1,3,[{file,"src/http_mini_tcp_serv.erl"},{line,118}
+            Acc=[Str|Acc];
+        K > 1 ->
+            Str1= pars_stn (string:tokens(H, ": ")),
+            Acc=[Str1|Acc]
+    end,
+    pars1(T, K+1, Acc).
+
+
+pars_st1 ([H,H2|_T]) ->
+    _Url2 =
+        if 
+            H == "GET" ->
+                       {H, H2};
+            true -> error
+        end.
+
+pars_stn ([H|T]) ->
+                {H, T}.
+
+
+
 
 
 %%%===================================================================
--- src/parsing.erl
+++ src/parsing.erl
@@ -3,65 +3,74 @@
 -export ([pars/1]).
 
 
-%% кто вызывает этот модуль? 
-%% кто-то должен увеличивать К
-%% go_recv?
-%% на сокет построчно приходит??
-%% -----------------------------
-
-%% pars (<<List>>, K) -> 
-%%     if 
-%%         K==1 ->
-%%             pars_st1 (binary:split (<<List>>, <<" ">>, [global]), []);
-%%         K > 1 ->
-%%             pars_st (binary:split(<<List>>, <<":">>), [])
-%%     end.     
-
 pars (List) -> 
     List2 = string:tokens(List, "\r\n"),
     io:format("List2=~p~n", [List2]),
-    pars1(List2, 1, []).
+    Asdf = pars1(List2, 1, []),
+    io:format("asdf = ~p~n", [Asdf]),
+    Asdf.
 
-pars1 ([], _K, Acc) -> Acc; %%нужно вернуть аккумулятор
+
+%% pars2([], _, Acc) ->
+%%     Acc;
+%% pars2([H|T], _, Acc) ->
+%%     case 
+%%         string:tokens(H, ": ")
+%%     of
+%%         Tokens when is_list(Tokens) andalso length(Tokens) == 2 ->
+%%             [H1|T1] = Tokens,
+%%             pars2(T, 1, [{H1,T1}|Acc]);
+%%         _Oth ->
+%%             io:format("other: ~p~n", [_Oth]),
+%%             pars2(T, 1, Acc)
+%%     end.
+
+
+pars1 ([], _K, Acc) -> lists:reverse(Acc);
 pars1 ([H|T], K, Acc) ->
+    io:format("H = ~p~n K=~p~n", [H, K]),
+    
+    Acc3 = if 
+               K==1 ->
+                   Str = pars_st1 (string:tokens(H, " ")),
+                   Acc2=[Str|Acc],
+                   io:format("Acc11: ~p~n", [Acc]),
+                   Acc2;
+               K > 1 ->
+                   Str1= pars_stn (string:tokens(H, " ")),
+                   Acc2=[Str1|Acc],
+                   io:format("Acc12: ~p~n", [Acc2]),
+                   Acc2
+           end,
+    io:format("Acc3: ~p~n", [Acc3]),
+    pars1(T, K+1, Acc3).
+
+
+pars_st1 ([H,H2|_T]) ->
     if 
-        K==1 ->
-            Str = pars_st1 (string:tokens(H, " ")),
-            Acc=[Str|Acc];
-        K > 1 ->
-            Str1= pars_stn (string:tokens(H, ": ")),
-            Acc=[Str1|Acc]
-    end,
-    pars1(T, K+1, Acc).
-
-
-pars_st1 ([H,H2|T]) ->
-    _Url2 =
-        if 
-            H == "GET" ->
-                if 
-                    T =="HTTP/1.0" -> 
-                       H2
-%%                        io:format("Location=~p~n", [H2]),
-                        ;
-                    true -> error
-                end;
-            true -> error
-        end.
+        H == "GET" ->
+            {get, H2};
+        true -> error
+    end.
 
 
 
 pars_stn ([H|T]) ->
-    _Url1 =
-        if 
-            H == "Host" ->
-                T;
-%%        io:format("Host=~p~n", [T]);
-            true -> error
-        end.
-
-
-
+    if 
+        H == "Host:" ->
+            {host, T};
+        H == "User-Agent:" ->
+            {useragent, T};
+        H == "Accept:" -> 
+            {accept, T};
+        H == "Accept-Language:" ->
+            {acceptlanguage, T};
+        H == "Accept-Charset:" ->
+            {acceptcharset, T};
+        true -> 
+            net_takoy_bukvi
+    end.
+        
 
 
 %% GET / HTTP/1.1
